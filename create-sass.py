ff1a20e114c0a78a5775705d7d8874108d20c9c2
amit_ <amitrai_13@yahoo.com>
2018-12-01 17:57:11 +0530

    folder creation to the same level.

--- create-sass.py
+++ create-sass.py
@@ -17,28 +17,28 @@ vendors=['_bootstrap.scss','_jquery.scss'];
 
 #to create all folder from folder list 
 for folder in folders:
-    os.makedirs('/sass/'+folder)
+    os.makedirs('sass/'+folder)
     
 #to create abstracts files inside folder abstracts 
 for files in abstractfiles:
-    open('/sass/abstracts/'+files,"w")
+    open('sass/abstracts/'+files,"w")
 
 for files in base:
-    open('/sass/base/'+files,"w")
+    open('sass/base/'+files,"w")
 
 for files in components:
     open('/sass/components/'+files,"w")
 
 for files in layout:
-    open('/sass/layout/'+files,"w")
+    open('sass/layout/'+files,"w")
 
 for files in pages:
-    open('/sass/pages/'+files,"w")
+    open('sass/pages/'+files,"w")
 for files in vendors:
-    open('/sass/vendors/'+files,"w")
+    open('sass/vendors/'+files,"w")
 for files in themes:
-    open('/sass/themes/'+files,"w")
+    open('sass/themes/'+files,"w")
     
-f= open("/sass/main.scss","w")
+f= open("sass/main.scss","w")
 f.write("@import 'abstracts/animations.scss'\n@import 'abstracts/mixins.scss'\n  @import 'abstracts/variables.scss'\n  @import 'abstracts/phaceholder.scss'\n@import 'components/buttons.scss'\n@import 'components/carousel.scss'\n@import 'components/cove.scss'\n@import 'components/dropdown.scss'\n@import 'layout/navigarion.scss'\n@import 'layout/grid.scss'\n@import 'layout/header.scss'\n@import 'layout/footer.scss'\n@import 'layout/sidebar.scss'\n@import 'layout/form.scss'\n@import 'pages/home.scss'\n@import 'pages/contact.scss'\n@import 'themes/theme.scss'\n")
 f.close()
